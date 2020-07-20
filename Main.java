ce12416b601f1369615b1a36e42db0266b43cfb9
Shreya Patel <67444978+shreya-patell@users.noreply.github.com>
2020-07-20 11:49:37 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the procedure for a cookie");
+        makeBatter ();
+        bake();
+        decorate();
+        System.out.println("Cookie is done");
+    }
+    public static void makeBatter (){
+        System.out.println("Lay the ingredients out");
+        System.out.println("Mix the pack in a bowl with butter and some Milk");
+        System.out.println("Mix well together");
+    }
+    public static void bake(){
+        System.out.println("Scoop cookies on parchment paper");
+        System.out.println("Bake cookies for 10 minutes on 420 degrees");
+    }
+    public static void decorate(){
+        System.out.println("Allow cookies to cool");
+        System.out.println("Spread frosting");
+    }
+}
